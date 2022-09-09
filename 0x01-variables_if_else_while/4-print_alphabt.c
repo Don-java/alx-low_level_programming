b373d7a0b39045bb4f4554f336de4bc44f581f14
Don-java <donijilad@gmail.com>
2022-09-09 15:05:44 -0700

    Print all the letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+
+* main - print if the number is postive, zero, or negative
+
+* Description: using the main function
+
+* this program prints "Programming is positive, zero, or negative
+
+* Return: 0
+
+*/
+
+int main(void)
+
+{
+
+charnch; 
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+
+{
+
+if (ch != 'e' && ch != 'q')
+
+{
+
+	putchar(ch);
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+
+)
