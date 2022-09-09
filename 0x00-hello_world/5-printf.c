e5e8089ca267d7e4ac86d1a985dd182650f3e5ec
Don-java <donijilad@gmail.com>
2022-09-08 18:38:56 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print the string in the put function 
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle 
+* Return: (0) 
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 
+{
+	print "Print with proper grammer, but the outcome is a piece of art,\n");[D[D[D[D[D[{
+	 printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
