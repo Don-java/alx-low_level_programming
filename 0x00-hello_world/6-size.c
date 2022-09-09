aa1e4e2fd396dcede44377849af8eca03b35a053
Don-java <donijilad@gmail.com>
2022-09-09 09:23:24 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** 
+* main - print the in the put function
+* 
+* main - print the in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle 
+* Return: 0  
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	print("size of an int: %ld byte(s)\n', sizeof(i));
+	printf("size of a long int: %ld byte(s)\n" sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	return (0);
