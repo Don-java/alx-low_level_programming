3e5335ef8251e02686c0199a01e7d569b981acc2
Don-java <donijilad@gmail.com>
2022-09-12 01:02:43 -0700

    Your program should end with the value

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-
 /**
 
- * main - Entry point
+ * main -entry point
 
  *
 
- * Return: always 0
+ * Return: 0 after printing the function
 
  */
+
+#include <stdio.h>
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
