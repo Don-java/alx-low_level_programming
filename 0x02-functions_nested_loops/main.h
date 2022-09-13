6ab929c88b78438cebc2b45465e1290e83ff4cb0
Don-java <donijilad@gmail.com>
2022-09-13 16:12:33 -0700

    Write a program

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-#ifndef HOLBERTON_N_ INCLUDED
-#define HOLBERTON_N_ INCLUDED
+#ifndef HORBERTON_N_INCLUDED
+#define HORBERTON_N_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
+int _isalpher(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -14,5 +14,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r,int n);
-#endi
+void num(int r, int n);
+
+#endif
