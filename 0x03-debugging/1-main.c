3cb9f78f7a563828f4ec37451a2f56411a6d63a9
Don-java <donijilad@gmail.com>
2022-09-14 05:12:18 -0700

    Your output should look like this.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,14 +7,17 @@
 
 int main(void)
 {
-int i;
-print("infinite loop incoming :(\n");
-i = 0;
-/*
-* while (i < 10>
-* {
-* putchar(i);
-* }
-*/
-print("infinite loop avoided! \\o/\n");
-return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
