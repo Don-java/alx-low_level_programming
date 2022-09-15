86214b016b5d6b60721bf27e124c4d396e7c3f6a
Don-java <donijilad@gmail.com>
2022-09-15 07:05:22 -0700

    Write a function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,12 @@
-Write a function that checks for uppercase character.
-Write a function that checks for a digit (0 through 9).
-Write a function that multiplies two integers
-Write a function that prints the numbers, from 0 to 9, followed by a new line.
-Write a function that prints the numbers, from 0 to 9, followed by a new line.
-Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
-Write a function that draws a straight line in the terminal.
-Write a function that draws a diagonal line on the terminal.
-Write a function that prints a square, followed by a new line.
-Write a function that prints a triangle, followed by a new line.
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int _mul((int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diogonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_numbers(int n);
