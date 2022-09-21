114b0b17fea849129075daf326f7a200b6a324b0
Don-java <donijilad@gmail.com>
2022-09-21 15:28:55 -0700

    Write a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns length as integer;
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
  */
-
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
+	int index = 0, src_len = 0;
 
-	while (*(s + len) != '\0')
-		len++;
+	while (src[index++])
+		src_len++;
 
-	return (len);
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
