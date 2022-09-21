6760d1abf1bee62de3bf320a6d24ebe1cc8f960c
Don-java <donijilad@gmail.com>
2022-09-21 15:40:29 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
-#include "main.h"
+#include "main.h
+#include <stdio.h>
 
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
+ * main - check the code for ALX School students.
  *
- * Return: A pointer to the resulting string dest.
+ * Return: Always 0.
  */
-char *_strncpy(char *dest, char *src, int n)
+int main(void)
 {
-	int index = 0, src_len = 0;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
-}
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strncat(s1, s2, 1);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s\n", ptr);
+	ptr = _strncat(s1, s2, 1024);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
