6a2ec02f4bc69b321ed016e3b80ea8370498ecbf
Don-java <donijilad@gmail.com>
2022-09-30 05:12:53 -0700

    A program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
-int main()  
-{  
-    int num1, num2;  
-    int sum, sub, mult, mod;  
-    float div;  
-  
-    /* 
-     * Read two numbers from user separated by comma 
-     */  
-    printf("Input any two numbers separated by comma : ");  
-    scanf("%d,%d", &num1, &num2);  
-  
-    /* 
-     * Performs all arithmetic operations 
-     */   
-    sum = num1 + num2;  
-    sub = num1 - num2;  
-    mult = num1 * num2; 
+#include <stdlib.h>
+#include "main.h"
 
- /* 
-     * Performs all arithmetic operations 
-     */   
-    sum = num1 + num2;  
-    sub = num1 - num2;  
-    mult = num1 * num2;  
-    div = (float)num1 / num2;  
-    mod = num1 % num2;  
-  
-    /* 
-     * Prints the result of all arithmetic operations 
-     */  
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
 
+int main(int argc, char *argv[])
+{
+	int i, j;
 
- printf("The sum of the given numbers : %d\n", sum);  
-    printf("The difference of the given numbers : %d\n", sub);  
-    printf("The product of the given numbers : %d\n", mult);  
-    printf("The quotient of the given numbers : %f\n", div);  
-    printf("MODULUS = %d\n", mod);  
-  
-    return 0;   
-} 
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
