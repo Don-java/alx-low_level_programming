78043794f9c7e113e342b473e1016705b4a96c9e
Don-java <donijilad@gmail.com>
2022-10-03 04:48:27 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,16 +1,13 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdlib.h>
 
-void *malloc_checked(unsigned int b);
-
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-
-void *_calloc(unsigned int nmemb, unsigned int size);
-
-int *array_range(int min, int max);
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char);
+char *creat_array(unsigned int size, char c);
+char *_strdup(char *sl, char *s2);
+int  **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char **strtow(char *str);
 
 #endif
